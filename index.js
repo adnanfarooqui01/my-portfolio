a3e1f7911f49eaa0e8a8cbb80b04599d10a07022
adnanfarooqui01 <adnanfarooqui811@gmail.com>
2025-08-14 15:51:47 +0530

    adding my portfolio to git hub

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById('scr4').addEventListener('click' ,function(){
+    document.getElementsByClassName("contact-section")[0].scrollIntoView({behavior : "smooth"})
+})
+document.getElementsByClassName('b1')[0].addEventListener('click' ,function(){
+    document.getElementsByClassName("contact-section")[0].scrollIntoView({behavior : "smooth"})
+})
+document.getElementById('scr3').addEventListener('click' ,function(){
+    document.getElementsByClassName("services-section")[0].scrollIntoView({behavior : "smooth"})
+})
+document.getElementById('scr2').addEventListener('click' ,function(){
+    document.getElementsByClassName("skills-section")[0].scrollIntoView({behavior : "smooth"})
+})
+document.getElementById('scr1').addEventListener('click' ,function(){
+    document.getElementsByClassName("about-section")[0].scrollIntoView({behavior : "smooth"})
+})
